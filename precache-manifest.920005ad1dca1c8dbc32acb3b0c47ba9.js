19026d4fa476fa6a11bcdf5658e4c6ef6d33e97f
Satyajit Ghana <satyajitghana7@gmail.com>
2020-04-09 07:07:12 +0530

    Updates

---
+++ precache-manifest.920005ad1dca1c8dbc32acb3b0c47ba9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c3df1d51ba1eb95d4647d7ef8665d0",
+    "url": "/ruas-lms/index.html"
+  },
+  {
+    "revision": "c97fe779eb478e4741af",
+    "url": "/ruas-lms/static/css/main.609b49cd.chunk.css"
+  },
+  {
+    "revision": "8f0736efcfeb1016cdfd",
+    "url": "/ruas-lms/static/js/2.03a979db.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/ruas-lms/static/js/2.03a979db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97fe779eb478e4741af",
+    "url": "/ruas-lms/static/js/main.6ca0e80c.chunk.js"
+  },
+  {
+    "revision": "47d4cacba27898eafc3f",
+    "url": "/ruas-lms/static/js/runtime-main.acff190e.js"
+  },
+  {
+    "revision": "2881d6da85bf73a1e6197e0db43578e7",
+    "url": "/ruas-lms/static/media/login-bg.2881d6da.jpg"
+  },
+  {
+    "revision": "b7d9acdda2681ecfe2bd6bbb19b159b5",
+    "url": "/ruas-lms/static/media/msruas_logo_symbol.b7d9acdd.png"
+  }
+]);
